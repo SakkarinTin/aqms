16ed8b6b1dd113d2901650819977968293414543
SakkarinTin <sakkarin.kmitl@gmail.com>
2020-11-11 22:52:16 +0700

    v3.0.1 Heroku Update - add files for hosting on Heroku
    v3.0 Front-end
    - Add Login Page Prototype /login
    - Add Register Page Prototype /register
    - Add Grafana iframe to Dashboard e.g. graphs
    - Add incomplete Grafana map to Dashboard
         Back-end
    - Login/Register Page implement
    - Change 'Station' -> 'Stations'
    - Add 'StationLogs' Table to the PostgreSql Database and link it to 'Stations' with 'primary_key'
    - Change Title from Hexiwarewebsite -> AQMS (Air Quality Monitoring System)

--- hexiwearwebsite/settings.py
+++ hexiwearwebsite/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (13.7, 100.5),
