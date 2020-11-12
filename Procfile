ab7f35c5057c645e82be28aef0f8702795e3c074
SakkarinTin <sakkarin.kmitl@gmail.com>
2020-11-12 17:19:25 +0700

    waitress

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hexiwearwebsite.wsgi --log-file -
+web: waitress-serve --port=$PORT hexiwearwebsite.wsgi:application
--- hexiwearwebsite/settings.py
+++ hexiwearwebsite/settings.py
@@ -1,5 +1,18 @@
 import django_heroku
 import os
+'''
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+
+'''
 current_path = os.getcwd()
 if os.name == 'nt':
     import platform
@@ -142,8 +155,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # [ERROR] Could not find the GDAL library
 #GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal204.dll'
-#GDAL_LIBRARY_PATH = current_path + r'\OSGeo4W64\bin\gdal204.dll'
-GDAL_LIBRARY_PATH = r'/app/OSGeo4W64/bin/gdal204.dll'
+GDAL_LIBRARY_PATH = current_path + r'\OSGeo4W64\bin\gdal204.dll'
+#GDAL_LIBRARY_PATH = r'/app/OSGeo4W64/bin/gdal204.dll'
 
 # Activate Django-heroku.
 django_heroku.settings(locals())
