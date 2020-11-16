77fadaeeb23ed4d921280624a221b4306377992b
SakkarinTin <sakkarin.kmitl@gmail.com>
2020-11-16 20:41:56 +0700

    V3.0.2 Heroku update - Adjust to hosting Heroku - Prepare to run on Linux (Ubuntu)
    v3.0.1 Heroku Update
    - add files for hosting on Heroku
    
    v3.0 Front-end
    - Add Login Page Prototype /login
    - Add Register Page Prototype /register
    - Add Grafana iframe to Dashboard e.g. graphs
    - Add incomplete Grafana map to Dashboard
         Back-end
    - Login/Register Page implement
    - Change 'Station' -> 'Stations'
    - Add 'StationLogs' Table to the PostgreSql Database and link it to 'Stations' with 'primary_key'
    - Change Title from Hexiwarewebsite -> AQMS (Air Quality Monitoring System)

--- hexiwearwebsite/settings.py
+++ hexiwearwebsite/settings.py
@@ -1,6 +1,6 @@
 import django_heroku
 import os
-'''
+
 if os.name == 'nt':
     import platform
     OSGEO4W = r"C:\OSGeo4W"
@@ -12,6 +12,7 @@ if os.name == 'nt':
     os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
     os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
+'''
 
 current_path = os.getcwd()
 if os.name == 'nt':
@@ -154,8 +155,8 @@ LEAFLET_CONFIG = {
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # [ERROR] Could not find the GDAL library
-#GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal204.dll'
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal204.dll'
+#GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 #GDAL_LIBRARY_PATH = current_path + r'/OSGeo4W64/bin/gdal204.dll'
 #GDAL_LIBRARY_PATH = r'/app/OSGeo4W64/bin/gdal204.dll'
