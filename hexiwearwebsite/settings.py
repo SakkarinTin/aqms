54d6b0f4993c11da72d7280f418bf25722015281
SakkarinTin <sakkarin.kmitl@gmail.com>
2020-12-07 20:56:43 +0700

    fix bug production1

--- hexiwearwebsite/settings.py
+++ hexiwearwebsite/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "/var/www/staticfiles/")
 
 LEAFLET_CONFIG = {
     'DEFAULT_CENTER': (13.7, 100.5),
